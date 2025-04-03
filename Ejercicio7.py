4b9f27a4af543ea6433755f608b6e210587205aa
Tiziano Martinelli <tmartinelli@alumnos.unsada.edu.ar>
2025-04-03 18:39:26 -0300

    feat: fix

--- Ejercicio7.py
+++ Ejercicio7.py
@@ -5,12 +5,12 @@ print("El numero ingresado es " + str(numero))
 if numero % 2 == 0:
     print("El numero es par")
     
-    for i in range(numero, numero * 2):
+    for i in range(numero, (numero * 2 + 1)):
         if i % 2 == 0:
             print(i)
 
 else:
     print("El numero es impar")
-    for i in range(numero, numero * 2):
+    for i in range(numero, (numero * 2 + 1)):
         if i % 2 != 0:
             print(i)
